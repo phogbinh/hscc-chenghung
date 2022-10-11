23aa9c1851cc91f0670eb3b5bf01dfb9b4f03b48
Bill Tran <phongbinh2511@gmail.com>
2022-10-11 16:02:26 +0800

    [master] added comments.

--- runExp.m
+++ runExp.m
@@ -1,5 +1,11 @@
 function runExp(TEST_POINT, MAX_SERVED, NUM_MSLOT, MAX_POWER_DUE_RUE, SEED, scenario)
-
+% TEST_POINT 使用者數量
+% MAX_SERVED 一個 RUE 可以服務最多幾個 DUEs（只有學長提出的方法才會用到
+% NUM_MSLOT 一個 time slot 要切成幾個 time minislots
+% MAX_POWER_DUE_RUE 論文 (C2) 的限制，先 DUE 再 RUE
+% SEED 隨機種子
+% scenario 使用的方法
+% e.g. runExp([4], 2, 2, [2, 2], [2:11], "proposed")
     for i = 1:length(SEED)
         for j = 1:length(TEST_POINT)
             clearvars -except TEST_POINT MAX_SERVED SEED scenario i j NUM_MSLOT MAX_POWER_DUE_RUE;
