f98385fa3ef7581d8f9f845c27d92bfd31cf48c1
Bill Tran <phongbinh2511@gmail.com>
2022-10-13 21:06:46 +0800

    [master] corrected profit when there is 0 relay DUEs.

--- get_profit.m
+++ get_profit.m
@@ -2,7 +2,7 @@
 function [res_RUE, res_DUEs, res_profit] = get_profit(RUE, relay_DUEs, DUEs)
   res_RUE = RUE.copy();
   res_DUEs = DUEs.copy();
-  res_profit = -1;
+  res_profit = 0;
   for mli = 1:length(relay_DUEs)
     [status, alloc_RUE, alloc_DUEs, profit, weight] = bSearch(res_RUE, relay_DUEs(mli));
     if ~strcmp(status, "Solved")
