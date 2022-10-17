0602992e91ba8d1b48ce9e9edf7ffe05809e187a
Bill Tran <phongbinh2511@gmail.com>
2022-10-17 19:31:34 +0800

    [master] corrected infeasible profit.

--- get_profit.m
+++ get_profit.m
@@ -6,7 +6,7 @@ function [res_RUE, res_DUEs, res_profit] = get_profit(RUE, relay_DUEs, DUEs)
   for mli = 1:length(relay_DUEs)
     [status, alloc_RUE, alloc_DUEs, profit, weight] = bSearch(res_RUE, relay_DUEs(mli));
     if ~strcmp(status, "Solved")
-      res_profit = -1;
+      res_profit = -inf;
       return;
     end
     res_RUE = alloc_RUE;
