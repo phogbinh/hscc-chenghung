949cc1c15575a4720f723fca31a2e032e124ff8a
HSCC MATLAB <hsccml-1080ti@hscc.nthu.edu.tw>
2022-12-22 16:57:08 +0800

    [master] updated runExp instruction.

--- runExp.m
+++ runExp.m
@@ -7,7 +7,7 @@ function runExp(TEST_POINT, RUEs_num, DUEs_num, MAX_SERVED, NUM_MSLOT, MAX_POWER
 % MAX_POWER_DUE_RUE 論文 (C2) 的限制，先 DUE 再 RUE
 % SEED 隨機種子
 % scenario 使用的方法
-% e.g. runExp([4, 5], [1, 2], [3, 3], 2, 2, [2, 2], [2:11], "proposed")
+% e.g. runExp([4, 5, 6, 7, 8], [1, 2, 2, 2, 2], [3, 3, 4, 5, 6], 2, 2, [2, 2], [2:35], "proposed")
     for i = 1:length(SEED)
         for j = 1:length(TEST_POINT)
             if strcmp(scenario, 'equal')
